4eee8cabffc06592873b3e07b55199ec30de1d35
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-04-12 16:42:54 +0900

    feat: Update existing class component as a functional component

--- webpack_practice_rock_scissors_paper/RockScissorsPaper.jsx
+++ webpack_practice_rock_scissors_paper/RockScissorsPaper.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const rspCoords = {
     rock: '0',
@@ -18,88 +18,62 @@ const computerChoice = (imgCoord) => {
     })[0];
 }
 
-class RockScissorsPaper extends Component {
-    
-    state = {
-        result: 'rock',
-        score:0,
-        imgCoord: '0',
-    }
+const RockScissorsPaper = () => {
+    const [result, setResult] = useState('');
+    const [score, setScore] = useState('');
+    const [imgCoord, setImgCoord] = useState(rspCoords.rock);
+    const interval = useRef();
 
-    interval;
 
-    componentDidMount(){
-        this.interval = setInterval(this.changeHand, 1000);
-    }
+    useEffect(() => { // componentDidMount, componentDidUpdate 역할(1대1 대응은 아님)
+        interval.current = setInterval(changeHand, 100);
+        return () => { // componentWillUnmount 역할 
+            clearInterval(this.interval.current);
+        }
+    }, [imgCoord]);
 
-    componentWillUnmount(){
-        clearInterval(this.interval);
-    }
-
-    changeHand = () => {
-        const { imgCoord } = this.state;
-        console.log('hello', this.state.imgCoord, rspCoords);
+    const changeHand = () => {
         if(imgCoord === rspCoords.rock) {
-            this.setState({
-                imgCoord: rspCoords.scissors,
-            });
+            setImgCoord(rspCoords.scissors);
         } else if(imgCoord === rspCoords.scissors){
-            this.setState({
-                imgCoord: rspCoords.paper
-            });
+            setImgCoord(rspCoords.paper);
         } else if(imgCoord === rspCoords.paper){
-            this.setState({
-                imgCoord: rspCoords.rock
-            });
+            setImgCoord(rspCoords.rock);
         }
     };
 
-    onClickBtn = (choice) => () => {
-        const { imgCoord } = this.state;
-        clearInterval(this.interval);
+    const onClickBtn = (choice) => () => {
+        clearInterval(interval.current);
         const myScore = scores[choice];
         const cpuScore = scores[computerChoice(imgCoord)];
         const diff = myScore - cpuScore;
         if(diff === 0){
-            this.setState({
-                result: '비겼습니다.'
-            });
+            setResult('비겼습니다.');
         } else if([-1, 2].includes(diff)){
-            this.setState((prevState) => {
-                return {
-                    result: '이겼습니다.',
-                    score: prevState.score + 1,
-                };
-            });
+            setResult('이겼습니다.');
+            setScore((prevScore) => prevScore + 1);
         } else {
-            this.setState((prevState) => {
-                return {
-                    result: '졌습니다.',
-                    score: prevState.score - 1,
-                };
-            });
+            setResult('졌습니다.');
+            setScore((prevScore) => prevScore - 1);
         }
         // 2초동안 결과를 확인하고 다시 재시작 
         setTimeout(() => {
-            this.interval = setInterval(this.changeHand, 1000);
+            interval.current = setInterval(changeHand, 100);
         }, 2000)
     };
 
-    render(){
-        const { result, score, imgCoord } = this.state;
-        return (
-            <>
-                <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
-                <div>
-                <button id="rock" className="btn" onClick={this.onClickBtn('rock')}>rock</button>
-                <button id="scissor" className="btn" onClick={this.onClickBtn('scissors')}>scissors</button>
-                <button id="paper" className="btn" onClick={this.onClickBtn('paper')}>paper</button>
-                </div>
-                <div>{result}</div>
-                <div>현재 {score}점</div>
-            </>
-        )
-    }
+    return {(
+        <>
+            <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
+            <div>
+            <button id="rock" className="btn" onClick={onClickBtn('rock')}>rock</button>
+            <button id="scissor" className="btn" onClick={onClickBtn('scissors')}>scissors</button>
+            <button id="paper" className="btn" onClick={onClickBtn('paper')}>paper</button>
+            </div>
+            <div>{result}</div>
+            <div>현재 {score}점</div>
+        </>
+    )}
 }
 
 export default RockScissorsPaper;
