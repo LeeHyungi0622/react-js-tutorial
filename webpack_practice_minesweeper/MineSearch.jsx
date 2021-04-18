7342a205448cde1724a7478fc0cf1dfcbb0ba79d
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-04-18 12:50:14 +0900

    feat: 클릭한 셀의 주변 셀도 주변에 지뢰가 없는 경우에는 같이 열어주도록 처리

--- webpack_practice_minesweeper/MineSearch.jsx
+++ webpack_practice_minesweeper/MineSearch.jsx
@@ -83,35 +83,96 @@ const reducer = (state, action) => {
         };
         case OPEN_CELL:{
             const tableData = [...state.tableData];
-            tableData[action.row] = [...state.tableData[action.row]];
-            tableData[action.row][action.cell] = CODE.OPENED;
-            // 주변 지뢰갯수 구하기
-            let around = [];
-            // 윗 줄이 존재하는 경우
-            // 윗줄의 세 줄을 검사항목에 넣어준다.
-            if(tableData[action.row - 1]){
+            // tableData[action.row] = [...state.tableData[action.row]];
+            // tableData[action.row][action.cell] = CODE.OPENED;
+            // 주변에 모든 빈칸을 검사해서 빈 칸인지 아닌지 검사를 해야되기 때문에 아래와 같이 검사를 한다.
+            // 따라서 모든 칸을 새로운 객체로 만들어준다.
+            tableData.forEach((row, i) => {
+                tableData[i] = [...row];
+            });
+            //  한 번 열었던 칸은 다시 열어보지 않도록 처리 (dynamic programming과 유사)
+            const checked = [];
+            const checkAround = (row, cell) => {
+                if([CODE.OPENED, CODE.FLAG_MINE, CODE.FLAG, CODE.QUESTION_MINE, CODE.QUESTION].includes(tableData[row][cell])){
+                    // 이미 열린 칸이나 지뢰가 있는 칸은 한번에 열면 안되기 때문에 막아준다.
+                    return;
+                }
+                // 상하좌우 칸이 아닌 경우를 필터링
+                if(row < 0 || row >= tableData.length || cell < 0 || cell >= tableData[0].length){
+                    return;
+                }
+                // 이미 검사한 칸인 경우
+                if(checked.includes(row + '/' + cell)){
+                    return;
+                } else {
+                    // 검사하지 않은 셀의 경우 checked 배열에 row/cell 형태로 push를 해준다.
+                    checked.push(row + '/' + cell);
+                }
+                // 주변 지뢰갯수 구하기
+                let around = [];
+                // 윗 줄이 존재하는 경우
+                // 윗줄의 세 줄을 검사항목에 넣어준다.
+                if(tableData[row - 1]){
+                    around = around.concat(
+                        tableData[row - 1][cell - 1],
+                        tableData[row - 1][cell],
+                        tableData[row - 1][cell + 1],
+                    );
+                }
+                // 현재 셀의 좌우 셀을 검사항목에 추가해준다.
                 around = around.concat(
-                    tableData[action.row - 1][action.cell - 1],
-                    tableData[action.row - 1][action.cell],
-                    tableData[action.row - 1][action.cell + 1],
+                    tableData[row][cell - 1],
+                    tableData[row][cell + 1],
                 );
-            }
-            // 현재 셀의 좌우 셀을 검사항목에 추가해준다.
-            around = around.concat(
-                tableData[action.row][action.cell - 1],
-                tableData[action.row][action.cell + 1],
-            );
-            // 아랫줄이 존재하는 경우
-            if(tableData[action.row + 1]){
-                around = around.concat(
-                    tableData[action.row + 1][action.cell - 1],
-                    tableData[action.row + 1][action.cell],
-                    tableData[action.row + 1][action.cell + 1],
-                );
-            }
-            // 검사항목들을 돌면서 지뢰가 있는 요소와 일치하는지 검사 
-            const count = around.filter((v) => [CODE.MINE, CODE.FLAG_MINE, CODE.FLAG_MINE].includes(v));
-            tableData[action.row][action.cell] = count.length;
+                // 아랫줄이 존재하는 경우
+                if(tableData[row + 1]){
+                    around = around.concat(
+                        tableData[row + 1][cell - 1],
+                        tableData[row + 1][cell],
+                        tableData[row + 1][cell + 1],
+                    );
+                }
+                // 검사항목들을 돌면서 지뢰가 있는 요소와 일치하는지 검사 
+                const count = around.filter((v) => [CODE.MINE, CODE.FLAG_MINE, CODE.FLAG_MINE].includes(v)).length;
+                tableData[row][action.cell] = count;
+
+                // 주변에 지뢰의 갯수가 하나도 없는 경우에 주변 셀을 열어준다. 
+                // 옆 셀이 빈칸인 경우 옆 셀을 열어주고, 또 그 옆 셀이 비어있는 경우 해당 셀의 옆 셀도 같이 열어준다.
+                // 재귀적으로 처리 
+                if(count === 0){
+                    if(row > -1){
+                        const near = [];
+                        // 제일 위칸을 클릭한 경우에 
+                        // 더이상 더 위칸이 없는 경우에 해당 셀을 없애준다.
+                        if(row - 1 > -1){
+                            near.push([row - 1, cell - 1]);
+                            near.push([row - 1, cell]);
+                            near.push([row - 1, cell + 1]);
+                        }
+                        near.push([row, cell - 1]);
+                        near.push([row, cell + 1]);
+                        // 제일 아랫칸을 클릭한 경우에
+                        // 더 이상의 아랫 칸은 없기 때문에 아래의 셀들을 없애준다.
+                        if(row + 1 < tableData.length){
+                            near.push([row + 1, cell - 1]);
+                            near.push([row + 1, cell]);
+                            near.push([row + 1, cell + 1]);
+                        }
+                        near.forEach((n) => {
+                            // 주변 칸들이 이미 열려있는 칸이 아니라면
+                            if(tableData[n[0]][n[1]] !== CODE.OPENED){
+                                checkAround(n[0], n[1]);
+                            }
+                        });
+                    }
+                } 
+                
+                
+            };
+
+            // 주변 셀 검사 함수를 이용해서 주변 셀 검사하기
+            checkAround(action.row, action.cell);
+            
             return {
                 ...state,
                 tableData,
